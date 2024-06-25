dae949518b3ce86d4b9c2568e99e68f28e62826e
Hadi <mohammad.hadi.hosseini81@gmail.com>
2024-06-25 17:37:50 +0330

    Use initState

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,16 +9,31 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
   @override
+  void initState() {
+    super.initState();
+    print('initState run');
+  }
+
+  int counter = 0;
+
+  @override
   Widget build(BuildContext context) {
+    print('build run');
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Choose a Location'),
-        centerTitle: true,
-        backgroundColor: Colors.blue[900],
-        elevation: 0,
-      ),
-      body: Text('choose location screen'),
-    );
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text('Choose a Location'),
+          centerTitle: true,
+          backgroundColor: Colors.blue[900],
+          elevation: 0,
+        ),
+        body: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
+          child: Text('counter is $counter'),
+        ));
   }
 }
