62425cb4a8d653255d3112bd47a16c05ec33a3a2
Hadi <mohammad.hadi.hosseini81@gmail.com>
2024-06-25 18:37:31 +0330

    Use Future and async-await keyword

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,17 +8,37 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async {
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'hadi';
+    });
+
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'musician';
+    });
+    print('$username - $bio');
+
+    // await Future.delayed(Duration(seconds: 3), () {
+    //   print('hadi');
+    // });
+
+    // Future.delayed(Duration(seconds: 2), () {
+    //   print('musician');
+    // });
+    // print('statment');
+  }
+
   @override
   void initState() {
     super.initState();
-    print('initState run');
+    getData();
+    print('hey there');
   }
 
   int counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    print('build run');
     return Scaffold(
         backgroundColor: Colors.grey[200],
         appBar: AppBar(
