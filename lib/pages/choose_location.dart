58ded8591c1c9d0aebc421091fa2b03e0844b2cf
Hadi <mohammad.hadi.hosseini81@gmail.com>
2024-06-26 12:17:19 +0330

    Get an API data from jsonplaceholder

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,52 +8,17 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'hadi';
-    });
-
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'musician';
-    });
-    print('$username - $bio');
-
-    // await Future.delayed(Duration(seconds: 3), () {
-    //   print('hadi');
-    // });
-
-    // Future.delayed(Duration(seconds: 2), () {
-    //   print('musician');
-    // });
-    // print('statment');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('hey there');
-  }
-
-  int counter = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          title: Text('Choose a Location'),
-          centerTitle: true,
-          backgroundColor: Colors.blue[900],
-          elevation: 0,
-        ),
-        body: ElevatedButton(
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
-          child: Text('counter is $counter'),
-        ));
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[900],
+        elevation: 0,
+      ),
+      body: Text('choose location'),
+    );
   }
 }
