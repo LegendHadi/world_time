364615ca1704afcbf31ab89a248065b65cb40aad
Hadi <mohammad.hadi.hosseini81@gmail.com>
2024-06-27 11:28:36 +0330

    Get worldtime API data and correct time format

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -11,12 +11,25 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
   void getData() async {
-    Response response =
-        await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+    Response response = await get(
+        Uri.parse('http://worldtimeapi.org/api/timezone/America/Toronto'));
     Map data = jsonDecode(response.body);
-    // print(response.body);
     print(data);
-    print(data['title']);
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 6);
+    print(datetime);
+    print(offset);
+    DateTime now = DateTime.parse(datetime);
+    if (data['utc_offset'].toString().startsWith('+')) {
+      now = now.add(Duration(
+          hours: int.parse(offset.split(':').first),
+          minutes: int.parse(offset.split(':').last)));
+    } else {
+      now = now.subtract(Duration(
+          hours: int.parse(offset.split(':').first),
+          minutes: int.parse(offset.split(':').last)));
+    }
+    print(now);
   }
 
   @override
