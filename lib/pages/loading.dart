eb7e3a106c04b656e5f50b9f0f2ee83e1a3aa704
Hadi <mohammad.hadi.hosseini81@gmail.com>
2024-06-27 17:59:41 +0330

    Set spinner of loading

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -29,7 +30,24 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading'),
+      backgroundColor: Colors.blue[900],
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'loading',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            ),
+          ),
+          SizedBox(height: 15),
+          SpinKitSpinningLines(
+            color: Colors.white,
+            size: 70.0,
+          ),
+        ],
+      ),
     );
   }
 }
